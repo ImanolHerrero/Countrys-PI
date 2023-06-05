c7e4cfcb878ba2e0e9be6bd352d453729987bc94
Ima <imano.arias@gmail.com>
2023-06-05 16:56:39 -0300

    relaciones entre Country y Activity (muchos a muchos), index router y routers creados

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,4 +2,11 @@ const { Router } = require("express");
 
 const router = Router();
 
+const countryRouter = require("./countryRouter");
+const activitiesRouter = require("./activitiesRouter");
+
+router.use('/countries', countryRouter);
+router.use('/activities', activitiesRouter);
+
+
 module.exports = router;
